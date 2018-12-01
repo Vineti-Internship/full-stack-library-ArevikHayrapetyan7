f61124f51573c6a7173a67139dff1dcc85552da0
ArevikHayrapetyan7 <43651907+ArevikHayrapetyan7@users.noreply.github.com>
2018-12-02 00:49:40 +0400

    Update autor.rb
    added validations and associations

--- lib-back/app/models/autor.rb
+++ lib-back/app/models/autor.rb
@@ -1,3 +1,7 @@
 class Autor < ApplicationRecord
-  
+  has_many:books
+  validates :name ,presence: true
+  validates :surname ,presence: true
+  has_many :books,dependent: :destroy
+validates_associated :books
 end
